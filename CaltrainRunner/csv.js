c5383ba918a18517ab2bf0a130d546046b34c84c
Robert Duffner <robertd@surveymonkey.com>
2014-10-16 17:40:26 -0700

    csv about to get moved

--- CaltrainRunner/csv.js
+++ CaltrainRunner/csv.js
@@ -5,26 +5,50 @@ var day = currentDate.getDate();
 var month = currentDate.getMonth() + 1;
 var year = currentDate.getFullYear();
 var formattedDate = month + "/" + day + "/" + year;
-var currentCaltrainServiceType;
 
-        
-function getCaltrainServiceType(currentDate, callback){
+function getCaltrainNorthBound(currentDate, stationID) {
+    getCaltrainServiceType(currentDate, stationID, getStopTimes);
+}
+
+
+function getCaltrainServiceType(currentDate, stationID, callback){
     weekday = new Array('sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday')
     dayOfWeek = weekday[currentDate.getDay()]
-    csv
+    var xxxxxx = csv
      .fromPath("data/caltrain/calendar.txt", {headers: true, discardUnmappedColumns: true})
      .on("data", function(data){
-         if (data[dayOfWeek] === '1'){
+        if (data[dayOfWeek] === '1'){
             serviceType = data.service_id
-         }
+        }
      })
      .on("end", function(){
-        callback(serviceType);
+        callback(serviceType, stationID);
      });
 }
 
-getCaltrainServiceType(currentDate, function(data){ currentCaltrainServiceType = data;});
-console.log(currentCaltrainServiceType)
+
+function getStopTimes(serviceType, stationID) {
+    console.log(serviceType);
+    var serviceIDPattern = '^[0-9]*-'
+    var re = new RegExp(serviceIDPattern);
+    csv
+     .fromPath("data/caltrain/stop_times.txt", {headers: true, discardUnmappedColumns: true})
+     .on("data", function(data){
+        var parsed_trip_id = data.trip_id.split(re)[1];
+         if (data.stop_id === stationID && parsed_trip_id === serviceType){
+            console.log(data);
+         }
+     })
+     .on("end", function(){
+         console.log("done");
+     });
+}
+
+getCaltrainNorthBound(currentDate, '70172');
+
+
+
+
 
 /*
 csv
