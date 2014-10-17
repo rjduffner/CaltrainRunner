a6698581c0497c9a5601579c4f5f6a6d272324c3
Louise <louise.s.fox@gmail.com>
2014-10-17 11:30:01 -0700

    timer

--- CaltrainRunner/public/javascripts/caltrain.js
+++ CaltrainRunner/public/javascripts/caltrain.js
@@ -1,5 +1,5 @@
 var trainData = { northbound: [
-	{   arrivalTime: "1500", 
+	{   arrivalTime: "1500",
         serviceType: "BB",
         tripHeadsign: 'HELLO WORLD',
         direction: "NORTH"
@@ -12,7 +12,7 @@ var trainData = { northbound: [
     }],
 
     southbound: [
- 	{   arrivalTime: "544", 
+ 	{   arrivalTime: "544",
         serviceType: "LI",
         tripHeadsign: 'HELLO WORLD',
         direction: "South"
@@ -22,27 +22,37 @@ var trainData = { northbound: [
         serviceType: "GIL",
         tripHeadsign: 'HELLO WORLD',
         direction: "South"
-    }]      
+    }]
 };
 
-$.get( "/get_departures")
-.done(function(data) {
-    currentDate = new Date();
-    for (item in data){
-        var arrivalDate = new Date(item.arrival_time)
-        var diff  =  new Date (arrivalDate.getTime() - currentDate.getTime());
-        item.diff = diff.getMinutes()
-    }
 
-        
+function get_departures() {
 
-    var finalTrainData = {'departing' : data}
-    var tpl = $('#table-template').html()
-    var html = Mustache.to_html(tpl, finalTrainData);
-    $('#train-data').html(html);
+    $.get( "/get_departures")
+    .done(function(data) {
+        currentDate = new Date();
+        for (item in data){
+            var arrivalDate = new Date(item.arrival_time)
+            var diff  =  new Date (arrivalDate.getTime() - currentDate.getTime());
+            item.diff = diff.getMinutes()
+        }
 
 
-});
+
+        var finalTrainData = {'departing' : data}
+        var tpl = $('#table-template').html()
+        var html = Mustache.to_html(tpl, finalTrainData);
+        $('#train-data').html(html);
+
+
+    });
+
+    setTimeout('get_departures()', 30000);
+    console.log("update");
+}
+
+get_departures();
+
 
 
 
